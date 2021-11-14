7d8758a04e2dd1d490c99e1949b424039e42a47e
codingfun02 <codingfun@naver.com>
2021-11-14 10:22:50 +0900

    #4 Changed order of  picked number

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
 const pickerForm = document.querySelector(".js-pickerForm"),
-    pickBtn = pickerForm.querySelector("button"),
-    startNumberInput = document.querySelector(".js-startNumberInput"),
-    endNumberInput = document.querySelector(".js-endNumberInput"),
-    pickedNumber = document.querySelector(".js-pickedNumber"),
-    remainingNumbers = document.querySelector(".js-remainingNumbers");
+  pickBtn = pickerForm.querySelector("button"),
+  startNumberInput = document.querySelector(".js-startNumberInput"),
+  endNumberInput = document.querySelector(".js-endNumberInput"),
+  pickedNumber = document.querySelector(".js-pickedNumber"),
+  remainingNumbers = document.querySelector(".js-remainingNumbers"),
+  pickerResetBtn = document.querySelector(".js-resetPicker");
 
-let startNumber, endNumber, randomNumbers = [];
+let startNumber,
+  endNumber,
+  randomNumbers = [];
 
 const handlePickAgainFormSubmit = (event) => {
-    event.preventDefault();
-    if(randomNumbers.length === 0) {
-        pickedNumber.innerText = "?";
-        setTimeout(() => {
-            alert("뽑을 숫자가 없습니다");
-        }, 0);
-        return;
-    }
-    const randomIndex = Math.floor(Math.random()* randomNumbers.length);
-    const randomNumber = randomNumbers[randomIndex];
-    pickedNumber.innerHTML = randomNumber;
-    randomNumbers.splice(randomIndex, 1);
-    remainingNumbers.innerText = `남은 숫자: ${randomNumbers.join(", ")}`;
+  event.preventDefault();
+  if (randomNumbers.length === 0) {
+    pickedNumber.innerText = "?";
+    setTimeout(() => {
+      alert("뽑을 숫자가 없습니다");
+    }, 0);
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * randomNumbers.length);
+  const randomNumber = randomNumbers[randomIndex];
+  pickedNumber.innerHTML = randomNumber;
+  randomNumbers.splice(randomIndex, 1);
+  remainingNumbers.innerText = `남은 숫자: ${randomNumbers.join(", ")}`;
 };
 
 const handlePickerFormSubmit = (event) => {
-    event.preventDefault();
-    const startNumberInputValue = startNumberInput.value,
-        endNumberInputValue = endNumberInput.value;
-    if (startNumberInputValue === "" || endNumberInputValue === "") {
-        return alert("숫자가 입력되지 않았습니다");
-    } else if (parseInt(startNumberInputValue) > parseInt(endNumberInputValue)) {
-        return alert("시작 숫자가 끝 숫자보다 더 큽니다");
-    }
-    startNumber = parseInt(startNumberInputValue);
-    endNumber = parseInt(endNumberInputValue);
-    for (let i = startNumber; i <= endNumber; i++) {
-        randomNumbers.push(i);
-    }
-    const randomIndex = Math.floor(Math.random() * randomNumbers.length);
-    const randomNumber = randomNumbers[randomIndex];
-    pickedNumber.innerText = randomNumber;
-    randomNumbers.splice(randomIndex, 1);
-    remainingNumbers.innerText = `남은 숫자: ${randomNumbers.join(", ")}`;
-    pickBtn.innerText = "다시 뽑기";
-    pickerForm.removeEventListener("submit",handlePickerFormSubmit);
-    pickerForm.addEventListener("submit", handlePickAgainFormSubmit);
-}
+  event.preventDefault();
+  const startNumberInputValue = startNumberInput.value,
+    endNumberInputValue = endNumberInput.value;
+  if (startNumberInputValue === "" || endNumberInputValue === "") {
+    return alert("숫자가 입력되지 않았습니다");
+  } else if (parseInt(startNumberInputValue) > parseInt(endNumberInputValue)) {
+    return alert("시작 숫자가 끝 숫자보다 더 큽니다");
+  }
+  startNumber = parseInt(startNumberInputValue);
+  endNumber = parseInt(endNumberInputValue);
+  for (let i = startNumber; i <= endNumber; i++) {
+    randomNumbers.push(i);
+  }
+  const randomIndex = Math.floor(Math.random() * randomNumbers.length);
+  const randomNumber = randomNumbers[randomIndex];
+  pickedNumber.innerText = randomNumber;
+  randomNumbers.splice(randomIndex, 1);
+  remainingNumbers.innerText = `남은 숫자: ${randomNumbers.join(", ")}`;
+  pickBtn.innerText = "다시 뽑기";
+  pickerForm.removeEventListener("submit", handlePickerFormSubmit);
+  pickerForm.addEventListener("submit", handlePickAgainFormSubmit);
+};
+
+const handlePickerResetBtnClick = () => {
+  startNumber = 0;
+  endNumber = 0;
+  randomNumbers = [];
+  startNumberInput.value = "";
+  endNumberInput.value = "";
+  remainingNumbers.innerText = "남은 숫자:";
+  pickBtn.innerText = "뽑기";
+  pickerForm.removeEventListener("submit", handlePickAgainFormSubmit);
+  pickerForm.addEventListener("submit", handlePickerFormSubmit);
+};
 
 pickerForm.addEventListener("submit", handlePickerFormSubmit);
+pickerResetBtn.addEventListener("click", handlePickerResetBtnClick);
